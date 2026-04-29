913de3c2e2f9076f306b4c22915f7e907570f222
Leandro Tullio <letullio@gmail.com>
2026-04-29 10:22:04 -0300

    Update API and AUTH URLs in env.js

--- frontend/src/assets/env.js
+++ frontend/src/assets/env.js
@@ -1,4 +1,4 @@
 window.__env = window.__env || {
-  API_URL: 'http://localhost:8080',
-  AUTH_URL: 'http://localhost:8080/auth'
+  API_URL: 'https://taller-backend-iwyn.onrender.com',
+  AUTH_URL: 'https://taller-backend-iwyn.onrender.com/auth'
 };

7d42501ef947755901e16269956ac340ae81a851
Leandro Tullio <lambda@MacBook-Pro-de-Leandro.local>
2026-05-21 18:01:56 -0300

    Register es-AR locale for Angular pipes

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,17 @@
+import { registerLocaleData } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import { MessageService } from 'primeng/api';
+import localeEsAr from '@angular/common/locales/es-AR';
 import Aura from '@primeuix/themes/aura';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { authInterceptor } from './app/core/auth/auth.interceptor';
+
+registerLocaleData(localeEsAr);
 
 bootstrapApplication(AppComponent, {
   providers: [
